00549ff71dcbf876465f13d8845a046fc94154ad
Jeevan Deep <g.one235@gmail.com>
2017-07-21 20:04:53 +0530

    first commit

---
+++ js/script1.js
@@ -0,0 +1,40 @@
+function hover(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/0bb21845706ca396eb7e761137108ba3.jpg');
+}
+function unhover(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/8310f92f8f773539da5e02ff905764ac.jpg');
+}
+function hove(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/a2c74bc9471b8d523953a46a25e722a5.jpg');
+}
+function unhove(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/84468cb9533b67071b3b624967ed1f91.jpg');
+}
+
+function hove2(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/fa4aed2ad34c3ac7ea02275ff2b30de2.jpg');
+}
+function unhove2(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/40a9bcea97313dcd630e4cec42da0e23.jpg');
+}
+function hove3(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/6cf3d25b49c3237cfd7b1a8d7b716b78.jpg');
+}
+function unhove3(element) {
+    element.setAttribute('src', 'https://www.fruute.com/uploads/images/thumbnails/de2d9ed86bbbb356b4303b65270a82c0.jpg');
+
+}
+function unhove4(element){
+	element.setAttribute('src',"https://www.fruute.com/uploads/images/thumbnails/559dea7008b8739f8e8f74163452db3e.jpg");
+}
+function hove4(element){
+	element.setAttribute('src',"https://www.fruute.com/uploads/images/thumbnails/f12af216792817ecf001bebd10337fdc.jpg");
+}
+
+function unhove5(element){
+	element.setAttribute('src',"https://www.fruute.com/uploads/images/thumbnails/b7080386045596b4e268fbbc0a4d27fd.jpg");
+}
+function hove5(element){
+	element.setAttribute('src',"https://www.fruute.com/uploads/images/thumbnails/e2237a2f2a8f3beb79fbad738918c24b.jpg");
+}
+
---
+++ js/script2.js
@@ -0,0 +1,12 @@
+	
+
+	function submitAnswers(event){
+		var name=document.getElementById('contact').value;
+ 
+       
+		if(name==''){
+			alert('please fill the name');
+		}
+
+
+	}
